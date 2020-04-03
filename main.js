bcb1ed9fa954637784b28ae9d38f0d942fa26a91
Zeke Lin <zeke5252@yahoo.com.tw>
2020-04-03 15:32:25 +0800

    Modified css, DIY the class method

--- main.js
+++ main.js
@@ -1,29 +1,26 @@
-
 class Img {
-    constructor(id) {
-      this.dom = document.getElementById(`${id}`);
-      this.fitProp = 'none';
-      this.dom.style.objectFit = this.fitProp
-    }
-    scaleToFit() {
-      this.fitProp = 'contain';  
-      this.dom.style.objectFit = this.fitProp
-    }
-    scaleToFill() {
-      this.fitProp = 'fill';  
-      this.dom.style.objectFit = this.fitProp
-    }
-    scaleToReset() {
-        this.fitProp = 'none';  
-        this.dom.style.objectFit = this.fitProp
-      }
+  constructor(id) {
+    this.dom = document.getElementById(`${id}`);
+    this.setFit("none");
   }
+  scaleToFit() {
+    this.setFit("contain");
+  }
+  scaleToFill() {
+    this.setFit("fill");
+  }
+  scaleToReset() {
+    this.setFit("none");
+  }
+  setFit(str) {
+    this.dom.style.objectFit = str;
+  }
+}
 
 (function init() {
-    let img1 = new Img('img1');
-    let img2 = new Img('img2');
-    let img3 = new Img('img3');
-    img2.scaleToFit();
-    img3.scaleToFill();
+  let img1 = new Img("img1");
+  let img2 = new Img("img2");
+  let img3 = new Img("img3");
+  img2.scaleToFit();
+  img3.scaleToFill();
 })();
-
